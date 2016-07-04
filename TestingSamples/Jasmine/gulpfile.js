de9bd39bd2f72c0d4ea57cf1891a5e2e393667bd
João Ronaldo Del-Ducca Cunha <joao.cunha@eldorado.org.br>
2016-07-04 14:04:37 -0300

    Added gulp-karma-runner

--- TestingSamples/Jasmine/gulpfile.js
+++ TestingSamples/Jasmine/gulpfile.js
@@ -1,9 +1,18 @@
 'use strict';
  
 var gulp = require('gulp'),
-    shell = require('gulp-shell');
+var Server = require('karma').Server;
  
-gulp.task('server', ['node', 'karma']);
+gulp.task('server', ['node', 'test']);
  
 gulp.task('node', shell.task('node app.js'));
-gulp.task('karma', shell.task('karma start "./karma.conf.js"'));
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
