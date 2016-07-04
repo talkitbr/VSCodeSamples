188ee40a3ca9e286713c1dbbc0da90fee78ad9eb
João Ronaldo Del-Ducca Cunha <joao.cunha@eldorado.org.br>
2016-07-04 13:45:57 -0300

    Move testing sample to test folder

--- Angular2TypescriptTest/app/main.ts
+++
@@ -1,6 +0,0 @@
-import 'reflect-metadata';
-
-import {bootstrap}    from 'angular2/platform/browser'
-import AppComponent from './app.component'
-
-bootstrap(AppComponent);
--- Angular2TypescriptTest/typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="assertion-error/assertion-error.d.ts" />
-/// <reference path="chai/chai.d.ts" />
-/// <reference path="es6-shim/es6-shim.d.ts" />
-/// <reference path="mocha/mocha.d.ts" />
